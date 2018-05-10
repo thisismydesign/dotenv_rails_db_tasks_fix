fd905545c0bf9ec9369f9c259ed7a1241332550a
Csaba Apagyi <csaba.apagyi@xing.com>
2018-05-10 12:13:09 +0200

    Sets up db rake tasks in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,17 @@
 require "bundler/setup"
 require "dotenv_rails_db_tasks_fix"
+require "active_record"
+require 'rake'
+
+RSPEC_ROOT = File.dirname __FILE__
+
+include ActiveRecord::Tasks
+DatabaseTasks.database_configuration = YAML.load_file(Pathname.new(RSPEC_ROOT).join("example_project", "config", "database.yml"))
+DatabaseTasks.root = Pathname.new(RSPEC_ROOT).join("example_project")
+DatabaseTasks.db_dir = 'db'
+DatabaseTasks.migrations_paths = [File.join(DatabaseTasks.root, 'db/migrate')]
+
+load 'active_record/railties/databases.rake'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure

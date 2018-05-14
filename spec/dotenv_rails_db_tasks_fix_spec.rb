093f87afe8175cf4840af694bccb8a9214954269
Csaba Apagyi <csaba.apagyi@xing.com>
2018-05-14 11:43:14 +0200

    Adds proper tests

--- spec/dotenv_rails_db_tasks_fix_spec.rb
+++ spec/dotenv_rails_db_tasks_fix_spec.rb
@@ -3,7 +3,15 @@ RSpec.describe DotenvRailsDbTasksFix do
     expect(DotenvRailsDbTasksFix::VERSION).not_to be nil
   end
 
-  it "can execute `rake db:create`" do
-    Rake::Task["db:create"].invoke
+  describe "rake db:create" do
+    after do
+      FileUtils.rm(Pathname.new(RSPEC_ROOT).join("example_project", "db", "development.sqlite3"))
+      FileUtils.rm(Pathname.new(RSPEC_ROOT).join("example_project", "db", "test.sqlite3"))
+    end
+
+    it "outputs DB creation message for 'development' and 'test' DBs" do
+      output = "Created database 'spec/example_project/db/development.sqlite3'\nCreated database 'spec/example_project/db/test.sqlite3'\n"
+      expect { Rake::Task["db:create"].invoke }.to output(output).to_stdout
+    end
   end
 end
